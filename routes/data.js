86401222ff4adf221cd8903481e78c3a12c4cc83
Scott Vandervort <scottv@qualcomm.com>
2018-03-21 13:23:12 -0700

    Added somore more CORS calls for React Playground.

--- routes/data.js
+++ routes/data.js
@@ -3,11 +3,27 @@ module.exports = function( cors ) {
   var express = require('express');
   var router = express.Router();
 
+  var sampleData = [{ "studio" : "Disney", "character" : "Mickey Mouse"},
+                    { "studio" : "Disney", "character" : "Donald Duck"},
+                    { "studio" : "Disney", "character" : "Goofy"},
+                    { "studio" : "Pixar", "character" : "Woody"},
+                    { "studio" : "Pixar", "character" : "Buzz Lightyear"}];
+
+
   // Simple example of a CORS-enabled POST.
-  router.post('/testCorsPost', cors(), function (req, res, next) {
-    console.log("Bang!");
+  router.post('/test', cors(), function (req, res, next) {    
     res.json({msg: 'Hello from server! This is a response from a CORS-enabled POST.'})
   })
+
+  // Simple example of a CORS-enabled GET.
+  router.get('/test', cors(), function (req, res, next) {    
+    res.json({msg: 'Hello from server! This is a response from a CORS-enabled GET.'})
+  })  
+
+  router.get('/characters', cors(), function (req, res, next) {    
+    res.json(sampleData);
+  })
+
     
   return  router;
 }
